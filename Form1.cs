882a33591ffa554f1c1b2fcb8bebcec150f26603
NARESH <64158150+NARESH-JPG@users.noreply.github.com>
2021-10-15 15:02:10 +0530

    Day_4

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace Demo_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SpeechSynthesizer sp = new SpeechSynthesizer();
+            sp.SetOutputToDefaultAudioDevice();
+            sp.Speak(richTextBox1.Text);
+        }
+    }
+}
